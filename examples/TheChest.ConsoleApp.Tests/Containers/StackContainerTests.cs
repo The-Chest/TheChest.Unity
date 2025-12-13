dcc05ebf4d984bc357c4458efc0389d9fff9520d
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2025-12-13 01:06:10 -0300

    Squashed 'Scripts/Core/' content from commit 7b0da9e
    git-subtree-dir: Scripts/Core
    git-subtree-split: 7b0da9e459ec84ea9f242954e38177f26aeae9ad

---
+++ examples/TheChest.ConsoleApp.Tests/Containers/StackContainerTests.cs
@@ -0,0 +1,27 @@
+﻿using TheChest.Core.Tests.Containers;
+
+namespace TheChest.ConsoleApp.Tests.Containers
+{
+    [TestFixtureSource(nameof(FixtureArgs))]
+    public class StackContainerTests : IStackContainerTests<Item>
+    {
+        static readonly object[] FixtureArgs = new object[]{
+            new object[] {
+                new StackContainerFactory<StackContainer, Item>(
+                    new StackSlotFactory<StackSlot, Item>()
+                ),
+                new SlotItemFactory<Item>(),
+            },
+            new object[] {
+                new StackContainerFactory<LazyStackContainer, Item>(
+                    new LazyStackSlotFactory<LazyStackSlot, Item>()
+                ),
+                new SlotItemFactory<Item>(),
+            },
+        };
+        public StackContainerTests(IStackContainerFactory<Item> containerFactory, ISlotItemFactory<Item> itemFactory)
+            : base(containerFactory, itemFactory)
+        {
+        }
+    }
+}
---
+++ src/TheChest.Core/Slots/Extensions/ISlotExtensions.cs
@@ -0,0 +1,24 @@
+﻿using TheChest.Core.Slots.Interfaces;
+
+namespace TheChest.Core.Slots.Extensions
+{
+    public static class ISlotExtensions
+    {
+        /// <summary>
+        /// Checks if the slot contains the item.
+        /// </summary>
+        /// <typeparam name="T">Type of item to be checked inside the slot</typeparam>
+        /// <param name="item">Item to be checked</param>
+        /// <returns>Returns true if <paramref name="item"/> is equal to <see cref="ISlot{T}.Content"/> and both are not null</returns>
+        /// <exception cref="ArgumentNullException">When <paramref name="item"/> is null</exception>
+        public static bool Contains<T>(this ISlot<T> slot, T item)
+        {
+            item = item ?? throw new ArgumentNullException(nameof(item));
+
+            if(slot.IsEmpty) 
+                return false;
+            
+            return slot.Content!.Equals(item);
+        }
+    }
+}
