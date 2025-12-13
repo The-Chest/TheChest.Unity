69a7846a0c6dc65c226d31eb1c28c180d68cf400
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2025-12-13 01:13:13 -0300

    Removes useless files from Script/Core

--- Scripts/Core/examples/TheChest.ConsoleApp/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using TheChest.ConsoleApp.Containers;
-
-var slots = Enumerable.Repeat(new Slot(), 10).ToArray();
-var container = new Container(slots);
-
-Console.Write($"Hey, this container has the size of {container.Size}");
